2fcfa4d2c67bd92fcfa418dd34340cd2bff4735d
aminasajid <aminasajid05@gmail.com>
2024-12-29 06:23:10 +0530

    favicon added

--- src/index.php
+++ src/index.php
@@ -6,6 +6,8 @@
     <title>Decor Haven</title>
     <link rel="stylesheet" href="output.css">
     <link rel="stylesheet" href="./assets/css/styles.css">
+    <!-- favicon -->
+     <link rel="shortcut icon" href="https://cdn-icons-png.flaticon.com/512/942/942165.png" type="image/x-icon">
 <!-- font awesome -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <!-- google fonts -->
